a573ededcb8e5593a7ded96554d3d474ce6e7caf
Sheldoor95 <jack_95@outlook.it>
2022-06-27 21:37:09 +0200

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Gian Marco Fantini on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
